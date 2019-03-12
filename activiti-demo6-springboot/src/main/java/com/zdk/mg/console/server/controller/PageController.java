4d548a64631bf440cedf0f64d6c7f54a206a9bcb
wangws <wangws@weigutech.com>
2019-03-12 13:52:56 +0800

    add resource

---
+++ activiti-demo6-springboot/src/main/java/com/zdk/mg/console/server/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.zdk.mg.console.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class PageController {
+	
+    @GetMapping("editor")
+    public String test(){
+        return "/modeler";
+    }
+}
